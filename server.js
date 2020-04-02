7609f77d45f15e82120f6960b97e2ddbabfd0733
Christian Sanchez <cas81695@gmail.com>
2020-04-02 19:49:03 -0400

    console on server

--- server.js
+++ server.js
@@ -22,6 +22,8 @@ app.use(passport.session());
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
+console.log('test');
+
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
